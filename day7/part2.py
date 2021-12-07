50b709e7bfa772f32278dc89675df89e6b20b63a
Olivier Leblanc <olivier.leblanc@croesus.com>
2021-12-07 08:22:16 -0500

    Added day 7

---
+++ day7/part2.py
@@ -0,0 +1,36 @@
+
+def calculate_fuel_for_target(list, target):
+    fuel = 0
+    for item in list:
+        # La nouvelle somme est rendu 
+        # S = n/2 * (p + d)
+        # n: nombre d'items
+        # p: premier item
+        # d: dernier item
+        fuel += int(abs(item - target)/2 * (1 + abs(item - target)))
+
+    return fuel
+
+def main(file_name):
+
+    numbers = []
+    with open(file_name, "r") as f:
+        l = f.read().split(",")
+
+        for x in l:
+            numbers.append(int(x))
+
+    min_number = min(numbers)
+    max_number = max(numbers)
+    best_fuel = calculate_fuel_for_target(numbers, min_number)
+    for possible_target in range(max_number + 1):
+        temp_fuel = calculate_fuel_for_target(numbers, possible_target)
+
+        if temp_fuel < best_fuel:
+            print(f"Le target '{possible_target}' est meilleur {temp_fuel}<{best_fuel}")
+            best_fuel = temp_fuel
+
+    print(best_fuel)
+
+if __name__ == "__main__":
+    main("7.input")
